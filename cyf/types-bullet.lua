26b1f7232647c38f1b3f405bebcf57899b6e60b1
polypoyo <evanpaulbishop@gmail.com>
2022-06-13 16:36:39 -0400

    Formatting

--- cyf/types-bullet.lua
+++ cyf/types-bullet.lua
@@ -1,8 +1,10 @@
 ---@class Bullet
 local Bullet = {}
+
 --- Gets a variable that you previously set using `Bullet.SetVar`. 
 ---@param key string
 function Bullet.GetVar(key)end
+
 ---Sets a variable on this bullet that you can retrieve with `Bullet.GetVar`. 
 ---@param value any
 ---@param key any
