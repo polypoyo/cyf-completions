4663a6a1b84b7584254751f869f6a08f215c5ce0
polypoyo <evanpaulbishop@gmail.com>
2022-06-13 16:25:12 -0400

    Add CreateProjectile

---
+++ cyf/api-projectile.lua
@@ -0,0 +1,6 @@
+--- Creates a bullet that you can store and modify, with its spawn position relative to the center of the arena. The hitbox of the bullet is a rectangle around the sprite, unless you use CYF's PP mode.
+---@param spritename string
+---@param initial_x number
+---@param initial_y number
+---@param layer string?
+function CreateProjectile(spritename, initial_x, initial_y, layer)end
