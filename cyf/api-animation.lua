bd2a7cf7fada38879f33244fc2be3e2c5146c2bf
polypoyo <evanpaulbishop@gmail.com>
2022-06-13 21:52:08 -0400

    Add CreateSprite

--- cyf/api-animation.lua
+++ cyf/api-animation.lua
@@ -8,3 +8,10 @@
 ---@field xscale number
 ---@field yscale number
 local sprite = {}
+
+--- Creates a sprite at the center of the screen (at 320, 240) that you can modify in many ways. 
+---@param spritename string
+---@param layer string?
+---@param childNumber number?
+---@return Sprite
+function CreateSprite(spritename, layer, childNumber)end
